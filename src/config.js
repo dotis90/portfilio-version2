b629e97a604f58a492ab08e4d4d53b30ecc77c22
dotis90 <dotis90@gmail.com>
2020-06-02 18:15:33 -0400

    Hero section

---
+++ src/config.js
@@ -0,0 +1,39 @@
+const heroInfo = {
+  firstName: "Daniel",
+  lastName: "Otis",
+  devDesc: "| Programmer | Web Developer | Game Designer | Writer | Teacher ",
+  icons: [
+    {
+      id: 0,
+      image: "fa-github",
+      url: "https://github.com/dotis90",
+      style: "socialicons",
+    },
+    {
+      id: 1,
+      image: "fa-facebook",
+      url: "https://www.facebook.com/dan.otis.10420",
+      style: "socialicons",
+    },
+    {
+      id: 2,
+      image: "fa-google",
+      url: "mailto:dotis90@gmail.com",
+      style: "socialicons",
+    },
+    {
+      id: 3,
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/daniel-otis-542043113/",
+      style: "socialicons",
+    },
+    {
+      id: 4,
+      image: "fa-twitter",
+      url: "https://www.twitter.com/Dotis90/",
+      style: "socialicons",
+    },
+  ],
+};
+
+export { heroInfo };
