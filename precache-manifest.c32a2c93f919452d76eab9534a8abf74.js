09ccf9578323a89961fc61484c1eb2215d81eec6
dotis90 <dotis90@gmail.com>
2020-06-12 17:17:23 -0400

    Updates

---
+++ precache-manifest.c32a2c93f919452d76eab9534a8abf74.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04229b2cac5ee91f244fdcc461c466c0",
+    "url": "/portfolio--v2/index.html"
+  },
+  {
+    "revision": "63d7bbbf2d5cfbdc5ac5",
+    "url": "/portfolio--v2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b0d86836918603327747",
+    "url": "/portfolio--v2/static/css/main.d932271b.chunk.css"
+  },
+  {
+    "revision": "63d7bbbf2d5cfbdc5ac5",
+    "url": "/portfolio--v2/static/js/2.9493de4d.chunk.js"
+  },
+  {
+    "revision": "e4c137de68f1dc6b72d1a2fbfaf8fb8f",
+    "url": "/portfolio--v2/static/js/2.9493de4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d86836918603327747",
+    "url": "/portfolio--v2/static/js/main.e4431183.chunk.js"
+  },
+  {
+    "revision": "a47f3b8fc25334390b72",
+    "url": "/portfolio--v2/static/js/runtime-main.16d7b800.js"
+  },
+  {
+    "revision": "541559a5ae6e5dc8746bebe93835ef9f",
+    "url": "/portfolio--v2/static/media/blog.541559a5.png"
+  },
+  {
+    "revision": "b183d8000f26184c53e221c3af2ba39b",
+    "url": "/portfolio--v2/static/media/cinematracker.b183d800.png"
+  },
+  {
+    "revision": "6f87591be99d2726e754be283d53eba5",
+    "url": "/portfolio--v2/static/media/movieapp.6f87591b.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio--v2/static/media/skillImage.59389695.svg"
+  }
+]);
